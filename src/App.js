bedbb350d6bbabced60fea65073e68a6b08fa595
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-01-06 19:57:09 +0530

    feat : handling login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import "./App.css";
-import { ChakraProvider } from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react";
 import { InputGroup } from "@chakra-ui/react";
 import { InputRightElement } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
-import react from "react";
 import { useState } from "react";
 function App() {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+
+  const [loginId, setLoginId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const apiUrl =
+        "https://qa2.sunbasedata.com/sunbase/portal/api/assignment_auth.jsp";
+
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({
+          login_id: loginId,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        // For simplicity, this example logs the access token to the console
+        console.log("Access Token:", data.access_token);
+        console.log(response);
+      } else {
+        // Handle authentication error
+        console.error("Authentication failed:", data.error);
+      }
+    } catch (error) {
+      console.error("Error during authentication:", error);
+    }
+  };
 
   return (
     <div className="App">
@@ -20,6 +52,7 @@ function App() {
           htmlSize={40}
           width="auto"
           className="login-id"
+          onChange={(e) => setLoginId(e.target.value)}
         />
         <div className="password">
           <InputGroup size="md">
@@ -27,6 +60,7 @@ function App() {
               pr="4.5rem"
               type={show ? "text" : "password"}
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
             />
             <InputRightElement width="4.5rem">
               <Button h="1.75rem" size="sm" onClick={handleClick}>
@@ -35,10 +69,10 @@ function App() {
             </InputRightElement>
           </InputGroup>
         </div>
-        <div className='submit-button'>
-        <Button colorScheme="blue" size="sm" >
-          Submit
-        </Button>
+        <div className="submit-button">
+          <Button colorScheme="blue" size="sm" onClick={handleSubmit}>
+            Submit
+          </Button>
         </div>
       </div>
     </div>
